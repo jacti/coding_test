e52b5bc6cd7e97aa81b36be835371201d52b76f8
jacti <jactio99@gmail.com>
2025-04-29 23:53:53 +0900

    [fin]swa/2001 prefix sum

--- sw_academy/diff2/2001.py
+++ sw_academy/diff2/2001.py
@@ -1,26 +1,24 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
+def calculate_prefix_sum(arr):
+    size = len(arr)
+    prefix_sum = [[0]*(size+1) for _ in range(size+1)]
+    for i in range(1,size+1):
+        for j in range(1,size+1):
+            prefix_sum[i][j] = prefix_sum[i][j-1] + prefix_sum[i-1][j] - prefix_sum[i-1][j-1] + arr[i-1][j-1]
+    return prefix_sum
+
 T = int(input())
 for test_case in range(1, T + 1):
     N, M = map(int, input().split())
     number_list = []
     for _ in range(N):
         number_list.append(list(map(int, input().split())))
-    row_filter = [[0]*(N-M+1) for _ in range(N)]
-    for i in range(N):
+    prefix_sum = calculate_prefix_sum(number_list)
+    filter = set()
+    for i in range(N-M+1):
         for j in range(N-M+1):
-            if j==0:
-                for k in range(M):
-                    row_filter[i][0] += number_list[i][k]
-            else:
-                row_filter[i][j] = row_filter[i][j-1] - number_list[i][j-1] + number_list[i][j+M-1]
-    filter = [[0]*(N-M+1) for _ in range(N-M+1)]
-    for j in range(N-M+1):
-        for i in range(N-M+1):
-            if i==0:
-                for k in range(M):
-                    filter[0][j] += row_filter[k][j]
-            else:
-                filter[i][j] = filter[i-1][j] - row_filter[i-1][j] + row_filter[i+M-1][j]
-    print(f"#{test_case} {max(map(max,filter))}")
+            filter.add(prefix_sum[i+M][j+M]-prefix_sum[i+M][j]-prefix_sum[i][j+M]+prefix_sum[i][j])
+
+    print(f"#{test_case} {max(filter)}")
