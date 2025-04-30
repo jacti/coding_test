a3967899f2df6135548b6471f55861414671550b
jacti <jactio99@gmail.com>
2025-04-30 10:26:31 +0900

    pass by python

--- sw_academy/diff2/1928.py
+++ sw_academy/diff2/1928.py
@@ -1,31 +1,26 @@
 import sys
 sys.stdin = open("input.txt", "r")
 
-characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcedfghijklmnopqrstuvwxyz0123456789+/'
+characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
 index = 0
 decoding_map = {}
-for chr in characters:
-    decoding_map[chr] = index
+for char in characters:
+    decoding_map[char] = index
     index+=1
 
 T = int(input())
 for test_case in range(1, T + 1):
     txt = input()
-    data = [0]
-    index = 0
-    swift = 18
-    for chr in txt:
-        data[index] += decoding_map[chr] << swift
-        swift -= 6
-        if swift < 0:
-            swift = 18
-            index +=1
-            data.append(0)
-    result = ""
-    for unit in data[:-1]:
-        chr1 = (unit & 0xff0000) >> 16
-        chr2 = (unit & 0xff00) >> 8
-        chr3 = unit & 0xff
-        result += str(bytes([chr1,chr2,chr3]),'utf-8')
-    print(f"#{test_case} {result}")
+    buffer = 0
+    bits = 0
+    decoded_text = ""
+    for char in txt:
+        buffer = (buffer<<6) + decoding_map[char]
+        bits +=6
+        if bits == 24:
+            while bits>0:
+                bits-=8
+                decoded_text += chr((buffer >> bits)&0xFF)
+            buffer = 0
+    print(f"#{test_case} {decoded_text}")
 
