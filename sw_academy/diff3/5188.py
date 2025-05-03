6603e1531edcd087d6cc10335c84cd0d09c3652a
jacti <jactio99@gmail.com>
2025-05-03 15:31:57 +0900

    [fin]5188 dp feat. BFS

--- sw_academy/diff3/5188.py
+++ sw_academy/diff3/5188.py
@@ -7,14 +7,20 @@ for test_case in range(1, T + 1):
     array = []
     for _ in range(N):
         array.append(list(map(int, input().split())))
-    
-    bfs = [[] for _ in range(2*N-1)]
-    bfs[0].append((0,0,array[0][0]))
+    dp = [[float("inf")]*N for _ in range(N)]
+    bfs = [set() for _ in range(2*N-1)]
+    bfs[0].add((0,0))
+    dp[0][0] = array[0][0]
     for level in range(2*N-1):
-        for node in bfs[level]:
-            if node[0] < N-1:
-                bfs[level+1].append((node[0]+1,node[1],node[2]+array[node[0]+1][node[1]]))
-            if node[1] < N-1:
-                bfs[level+1].append((node[0],node[1]+1,node[2]+array[node[0]][node[1]+1]))
-    result = min(bfs[2*N-2],key=lambda x: x[2])[2]
-    print(f"#{test_case} {result}")
+        for y,x in bfs[level]:
+            if y < N-1:
+                new_length = dp[y][x] + array[y+1][x]
+                if new_length < dp[y+1][x]:
+                    bfs[level+1].add((y+1,x))
+                    dp[y+1][x] = new_length
+            if x < N-1:
+                new_length = dp[y][x] + array[y][x+1]
+                if new_length < dp[y][x+1]:
+                    bfs[level+1].add((y,x+1))
+                    dp[y][x+1] = new_length
+    print(f"#{test_case} {dp[N-1][N-1]}")
