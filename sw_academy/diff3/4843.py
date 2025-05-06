69e0c3974c6b55708fdc1497fea86d06a0e276ba
jacti <jactio99@gmail.com>
2025-05-06 19:27:30 +0900

    [error] 8/10 time out maybe

---
+++ sw_academy/diff3/4843.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+T = int(input())
+for test_case in range(1, T + 1):
+    N = int(input())
+    array = list(map(int, input().split()))
+    array.sort(reverse=True)
+    i = 0
+    result = []
+    for j in range(len(array)):
+        if j%2 == 0:
+            result.append(array[i])
+            i +=1
+        else:
+            result.append(array[-i])
+    print(f"#{test_case} {' '.join(map(str,result))}")
